0bebce7538303f69c6767e7d8ae0b86f616e5c44
brunoreis1307 <brunonicolas1307@gmail.com>
2025-12-01 11:05:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.set_page_config(page_title="Power Routine", page_icon="⚡", layout="centered
 st.title("Power Routine ⚡ — Módulo Dietas")
 
 st.subheader("Bem-vindo ao Power Routine")
-st.write("Aqui será exibida a interface principal do seu site.")
+st.write("Acesse as funcionalidades no painel à esquerda")
 
 
